56a68c1a750bbbfccf22feaec9203a05ccb1d7bf
Pheng <phengyangster9@gmail.com>
2022-04-19 15:41:26 -0700

    4.1

---
+++ 4_1.py
@@ -0,0 +1,37 @@
+
+# def get_number():
+#     return 5
+# def set_number(num):
+#     number = num 
+#     print("From set_number, num is: ", str(num))
+#     return number
+# num = get_number() 
+# set_number(6)
+# print("End of program. Num is:", str(num))
+
+# def add_number(num1, num2):
+#     val = num1 + num2
+#     print("from add_number, val is: ", str(val))
+
+# output = add_number(2, 5) # 7
+# print("Output of {} and {} is {}".format(str(2), str(5), output))
+
+# def get_firstname(full_name): #Pheng Yang
+#     space = full_name.index(" ")
+#     first_name = full_name [:space]
+#     return first_name
+# my_name = get_firstname("Pheng Yang")
+# print("Hello, my name is " + my_name + ".") # Pheng
+
+# names = ["Lucas Phan", "Joe Biden", "Mike Tyson", "Tom Brady", "Kobe Bryant"]
+# for name in names: 
+#     my_name = get_firstname(name)
+#     if my_name == "Tom":
+#         print(my_name + " plays football!")
+#     elif my_name == "Kobe":
+#         print(my_name + " plays basketball!")
+#     elif my_name == "Mike":
+#         print(my_name + " is into boxing")
+#     else: 
+#         print(my_name + " is not into sports")
+
